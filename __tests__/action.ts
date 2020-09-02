28b92fd015ba2a3d765d59074f5798f5798d4a20
Philipp Korber <philipp@korber.dev>
2020-09-02 03:49:33 +0200

    Wired input parsing and inner-api calls together to implement action.ts

--- __tests__/action.ts
+++ __tests__/action.ts
@@ -1,5 +1,116 @@
-describe("TODO", () => {
-    test("TODO", () => {
-        return
+jest.mock("../src/gh-inputs")
+jest.mock("../src/inner-api")
+jest.mock("@actions/core")
+
+import * as _core from "@actions/core"
+import { mocked } from "ts-jest/utils"
+import { run_action } from "../src/action"
+import { parseGithubActionInputs as _parseGithubActionInputs } from "../src/gh-inputs"
+import { CollectTagsFromGitOption } from "../src/git"
+import { TrustLevel } from "../src/gpg"
+import { collectAndVerify as _collectAndVerify, Options } from "../src/inner-api"
+
+const parseGithubActionInputs = mocked(_parseGithubActionInputs)
+const collectAndVerify = mocked(_collectAndVerify)
+const core = mocked(_core, true)
+
+const mockOptions: Options = {
+    collection: {
+        for_commits: {
+            explicitly_include: ["abc"],
+            include_in_range: { from_ref: "/ref/from", to_ref: "/ref/to" },
+            explicitly_exclude: ["def"]
+        },
+        for_tags: {
+            explicitly_include: ["v0"],
+            include_from_git: CollectTagsFromGitOption.All,
+            explicitly_exclude: ["v102"],
+            filter_regex: null
+        }
+    },
+    verification: {
+        for_commits: {
+            ignoreUnknownKeys: false,
+            ignoreUntrustedKeys: false,
+            requireMinTrustLevel: TrustLevel.Marginal,
+            requireSignature: false
+        },
+        for_tags: {
+            ignoreUnknownKeys: false,
+            ignoreUntrustedKeys: false,
+            requireMinTrustLevel: TrustLevel.Full,
+            requireSignature: true
+        }
+    }
+}
+
+beforeEach(() => {
+    jest.resetAllMocks()
+})
+
+//TODO change includedCommits/Tags to accept JSON array instead of
+//     stringly array.
+
+describe("run_action", () => {
+    test("calls the inner api with parsed inputs", async () => {
+        parseGithubActionInputs.mockReturnValueOnce(mockOptions)
+        collectAndVerify.mockResolvedValueOnce({
+            tags: new Set(),
+            commits: new Set(),
+            errors: []
+        })
+
+        await run_action()
+
+        expect(parseGithubActionInputs).toHaveBeenCalledTimes(1)
+        expect(collectAndVerify).toHaveBeenCalledWith(mockOptions)
+        expect(collectAndVerify).toHaveBeenCalledTimes(1)
+    })
+
+    test("all error messages are logged with core.error", async () => {
+        parseGithubActionInputs.mockReturnValueOnce(mockOptions)
+        collectAndVerify.mockResolvedValueOnce({
+            tags: new Set(),
+            commits: new Set(),
+            errors: [new Error("msg1"), new Error("msg2")]
+        })
+
+        await run_action()
+
+        expect(core.error.mock.calls).toEqual([["msg1"], ["msg2"]])
+    })
+
+    test("if there are errors setFailed is called", async () => {
+        parseGithubActionInputs.mockReturnValueOnce(mockOptions)
+        collectAndVerify.mockResolvedValueOnce({
+            tags: new Set(),
+            commits: new Set(),
+            errors: [new Error("msg1"), new Error("msg2")]
+        })
+
+        await run_action()
+
+        expect(core.setFailed).toHaveBeenCalledWith(
+            "Signature verifications failed in at least one case."
+        )
+        expect(core.setFailed).toHaveBeenCalledTimes(1)
+    })
+
+    test("if there are no errors setOutput is used to set commits and tags output", async () => {
+        parseGithubActionInputs.mockReturnValueOnce(mockOptions)
+        collectAndVerify.mockResolvedValueOnce({
+            tags: new Set(["v0", "v1"]),
+            commits: new Set(["commit0", "commit1"]),
+            errors: []
+        })
+
+        await run_action()
+
+        expect(core.setOutput).toHaveBeenCalledWith(
+            "verified-commits",
+            new Set(["commit0", "commit1"])
+        )
+        expect(core.setOutput).toHaveBeenCalledWith("verified-tags", new Set(["v0", "v1"]))
+        expect(core.setOutput).toHaveBeenCalledTimes(2)
     })
 })
--- src/action.ts
+++ src/action.ts
@@ -1,3 +1,22 @@
+import * as core from "@actions/core"
+import { parseGithubActionInputs } from "./gh-inputs"
+import { collectAndVerify } from "./inner-api"
+
+/** runs the action parsing inputs, verifying tags and commits and reporting the result */
 export async function run_action(): Promise<void> {
-    return
+    const options = parseGithubActionInputs()
+    const results = await collectAndVerify(options)
+
+    for (const error of results.errors) {
+        core.error(error.message)
+    }
+
+    if (results.errors.length > 0) {
+        core.setFailed("Signature verifications failed in at least one case.")
+    } else {
+        //Only set output is not failed as this contains all checked
+        //commits and tags, not only the successful checked commits and tags.
+        core.setOutput("verified-commits", results.commits)
+        core.setOutput("verified-tags", results.tags)
+    }
 }
